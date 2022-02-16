a246f0712567c2726fb7b0b9c8e2e3537531a96f
msoeker <80034462+msoeker@users.noreply.github.com>
2022-02-16 10:11:36 +0100

    building components

---
+++ starter/05-Design/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const newBtn = document.querySelector('.btn');
+console.log("hello");
+newBtn.addEventListener("click", clicked);
+
+function clicked(){
+  console.log("Button wurde geclickt");
+}
